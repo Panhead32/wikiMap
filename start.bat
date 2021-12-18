fa313df0796e780ff42eda81f4a649b05da900df
Roman <54202786+Panhead32@users.noreply.github.com>
2021-12-18 12:28:50 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 ﻿@echo off
-start cmd /k "cd client && npm start" 
-start cmd /k "cd server && npm run start" 
+start cmd /k "cd client && npm i && npm start" 
+start cmd /k "cd server && npm i && npm run start" 
 exit
