debef203ffdf7f2ef9def6c7ca8a1b68dcb6498e
Roman <orientir116@gmail.com>
2021-12-20 03:55:02 +0200

    add markers

--- client/src/components/Main/Map.vue
+++ client/src/components/Main/Map.vue
@@ -4,41 +4,56 @@
 
 <script>
 
-const { ol, axios } = window
+const { ol } = window
 export default {
   name: 'Map',
   data () {
     return {
-      map: null,
-      points: new ol.layer.VectorLayer({
-        source: this.vectorSource
-      })
+      map: null
     }
   },
   mounted () {
     this.init()
-    this.getAllPoints()
     this.clickHadler()
   },
   methods: {
-    async getAllPoints () {
-      const { data } = await axios.get('http://localhost:3001/points/all')
-      const coordinates = data.map((el) => el.coordinates)
-      console.log(coordinates)
-    },
     init () {
       this.map = new ol.Map({
         target: 'map',
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
-          })
+          }),
+          this.setPoints()
         ],
         view: new ol.View({
           center: ol.proj.fromLonLat([37.41, 8.82]),
           zoom: 4
         })
       })
+    },
+    setPoints () {
+      const rome = new ol.Feature({
+        geometry: new ol.geom.Point(ol.proj.fromLonLat([12.5, 41.9]))
+      })
+      rome.setStyle(
+        new ol.style.Style({
+          image: new ol.style.Icon({
+            color: '#BADA55',
+            crossOrigin: 'anonymous',
+            // For Internet Explorer 11
+            imgSize: [20, 20],
+            src: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAAFVBMVEX///8AAABQUFBTU1Po6OhfX18XFxfPXEYCAAABbklEQVR4nO3dsW0DQRAEQYpPMv+Q5ckf48TGoyqAwTVw/j4eAAAAAAAAAAAA3NPr/Wx4vw4Vvn8q3ocKn98O+/M8V/i5vv1Dn9fnaOF1aHpxHS08Nb04+AyF/0Rhc3qhsDm9UNicXihsTi8UNqcXCpvTC4XN6YXC5vRCYXN6obA5vVDYnF4obE4vFDanFwqb0wuFzemFwub0QmFzeqGwOb1Q2JxeKGxOLxQ2pxcKm9MLhc3phcLm9EJhc3qhsDm9UNicXihsTi8UNqcXCpvTC4XN6YXC5vRCYXN6obA5vVDYnF4obE4vFDanFwqb0wuFzemFwub0QmFzeqGwOb1Q2JxeKGxOLxQ2pxcKm9MLhc3phcLm9EJhc3qhsDm9UNicXihsTi8UNqcXCpvTC4XN6YXC5vRCYXN6obA5vVDYnF4obE4vFDanFwqb0wuFzenF2cL737C8/x3SiFOF978HfP+bzgAAAAAAAAAAAHzbL8RzDOHxqGGaAAAAAElFTkSuQmCC'
+          })
+        })
+      )
+      const vectorSource = new ol.source.Vector({
+        features: [rome]
+      })
+      const VectorLayer = new ol.layer.Vector({
+        source: vectorSource
+      })
+      return VectorLayer
     },
     clickHadler () {
       this.map.on('click', (data) => {
--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,10 @@ const bodyParser = require('body-parser')
 const config = require('../config/config')
 const mainPage = require('./router/router')
 const pointActions = require('./router/mapObject')
+const cors = require('cors')
 
 const app = express()
-
+app.use(cors())
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/', mainPage)
